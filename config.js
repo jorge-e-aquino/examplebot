d0c68200c57f583b7fb2764e21e821af48f48712
Jorge Aquino <jaquino6286@gmail.com>
2020-10-28 14:29:25 -0400

    post a tweet

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // Put your own Twitter App keys here. See README.md for more detail.
 module.exports = {
-  consumer_key:         'blah',
-  consumer_secret:      'blah',
-  access_token:         'blah',
-  access_token_secret:  'blah'
+  consumer_key: '5OBg5v7OfZodET2DLT4r61yMF',
+  consumer_secret: 'x0XzhTl4BfCh0L7yWhryFz93DbRVMh5mTF6rzN85ePwwSZE4nN',
+  access_token: '1321181147301380103-h8LYDy2kUud4mQrCst5Nkg9udRsf8U',
+  access_token_secret: 'yTv5QfT1TvdchCYQUIf6AuvHc6ac6b4awinxmaUYwfqPJ'
 }
